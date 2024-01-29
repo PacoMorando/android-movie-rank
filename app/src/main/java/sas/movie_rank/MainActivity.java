819448adfb7ab1619fd80f56a8f91d624e8e0885
NYP_PC <paco.morando@gmail.com>
2024-01-28 19:13:49 -0600

    Setting databinding

--- app/src/main/java/sas/movie_rank/MainActivity.java
+++ app/src/main/java/sas/movie_rank/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import sas.movie_rank.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        this.binding.setMain(this);
+        setContentView(this.binding.getRoot());
+        //setContentView(R.layout.activity_main);
+        this.binding.title.setText("Hola data binding");
     }
 }
